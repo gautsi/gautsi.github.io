6bd804a7b6723d6b5b0c536e3e06e888de0f4078
Gautam <gautam.sisodia@gmail.com>
2018-07-08 13:49:37 -0400

    updated simple rl

--- sketches/simple_rl/simple_rl.js
+++ sketches/simple_rl/simple_rl.js
@@ -1,178 +1,258 @@
-let states = [];
-const actions = ['up', 'down', 'left', 'right'];
-const posUpdAmnt = 0.005;
-let rand = 1;
-let randDecayFactor = 1.005;
-
+const boardLength = 10;
+let games = [];
+const discount = 0.9;
 let losses = [];
-let maxLoss = 0;
+let maxLossPlot = 500;
+const numGames = 100;
+const numTrainGames = 10;
 
-let rewards = [];
-let maxReward = 0;
+const plotModelQValues = [0, 5, 9];
+const modelQValueTensor = makeModelQValueTensor(plotModelQValues);
+let modelQValues;
 
+let xt;
+let yt;
+let qvaluesxt;
+
+let playerPos;
+let targetPos;
 
 // defining the model
-// input = ball pos + target pos + action
-// output = predicted reward
 let model;
-const layerSizes = [8, 4, 4, 1];
+const layerSizes = [3, 3, 3, 1];
 const activations = ['tanh', 'tanh', 'tanh'];
 const learningRate = 0.01;
 const optimizer = tf.train.adam(learningRate);
+const loss = 'meanSquaredError';
 
 function setup() {
   let myCanvas = createCanvas(400, 400);
   myCanvas.parent('simple_rl_sketch');
-  states.push({
-    'ball_pos': [0.5, 0.5], // getRandPos(),
-    'target_pos': [0.5, 0.5] // getRandPos()
+
+  console.log(modelQValueTensor);
+
+  model = makeModel(layerSizes, activations, optimizer, loss);
+  startNewGame();
+  setTimeout(getQValues, 10);
+  setTimeout(makeTensors, 10);
+  setTimeout(trainModel, 10);
+  setTimeout(getModelQValues, 10);
+}
+
+function makeModelQValueTensor(plotModelQValues) {
+  let xs = [];
+  for (let i = 0; i < plotModelQValues.length; i ++) {
+    for (let j = 0; j < boardLength; j ++) {
+      xs.push(...[0, 1].map(e => [j / boardLength,  plotModelQValues[i] / boardLength, e]));
+    }
+  }
+  return tf.tensor(xs);
+}
+
+function getModelQValues() {
+  tf.tidy(() => {
+    model.predict(modelQValueTensor).data().then(result => {modelQValues = result});
   });
-  model = makeModel(layerSizes, activations);
+  setTimeout(getModelQValues, 10);
 }
 
-function getRandPos() {
-  return [random(), random()];
+function startNewGame() {
+  playerPos = floor(random(0, boardLength));
+  targetPos = floor(random(0, boardLength));
+  games.push({
+    "states": [{
+      "player pos": playerPos,
+      "target pos": targetPos
+    }],
+    "actions": [],
+    "rewards": [],
+    "qvalues": [],
+    "ended": false
+  });
 }
 
-function drawBall(states) {
-  noStroke();
-  fill(myDarkColors[1]);
-  ellipse(
-    map(states[states.length - 1]['ball_pos'][0], 0, 1, 0, width),
-    map(states[states.length - 1]['ball_pos'][1], 0, 1, 0, height),
-    10);
-}
+function getQValues() {
 
-function drawTarget(states) {
-  noStroke();
-  fill(makeColor(myDarkColors[2]));
-  ellipse(
-    map(states[states.length - 1]['target_pos'][0], 0, 1, 0, width),
-    map(states[states.length - 1]['target_pos'][1], 0, 1, 0, height),
-    10);
-}
-
-function oneHot(action, actions) {
-  let vec = [];
-  for (let i = 0; i < actions.length; i ++) {
-    if (actions[i] == action) {
-      vec.push(1);
-    }
-    else {
-      vec.push(0);
-    }
+  if (qvaluesxt) {
+    qvaluesxt.dispose();
   }
-  return vec;
+
+  qvaluesxt = tf.tensor([0, 1].map(e => [playerPos / boardLength, targetPos / boardLength, e]));
+  tf.tidy(() => {
+    model.predict(qvaluesxt).data().then(result => {move([result[0], result[1]])});
+  });
 }
 
-function getModelInputArr(state, action, actions) {
-  return state['ball_pos'].concat(state['target_pos']).concat(oneHot(action, actions));
-}
 
-function getBestAction(actions, rewards) {
-  let best = 0;
-  for (let i = 1; i < rewards.length; i ++) {
-    if (rewards[i] > rewards[best]) {
-      best = i;
-    }
-  }
-  return actions[best];
-}
-
-function getAction(actions, rewards, rand) {
-  if (random() < rand) {
-    return random(actions);
+function move(qvalues) {
+  if (games[games.length - 1]["ended"]) {
+    startNewGame();
   }
   else {
-    return getBestAction(actions, rewards);
+    let currGame = games[games.length - 1];
+    let currState = currGame["states"][currGame["states"].length - 1];
+    let action = getAction(qvalues, randFactor = max(0, 1 - 0.01 * games.length));
+    playerPos = getNewPos(action);
+    let reward = getReward();
+    currGame["states"].push({"player pos": playerPos, "target pos": targetPos});
+    currGame["actions"].push(action);
+    currGame["rewards"].push(reward);
+    currGame["qvalues"].push(qvalues);
+    if (reward == 1 || currGame["states"].length > 20) {
+      currGame["ended"] = true;
+    }
+  }
+  if (games.length < numGames) {
+    setTimeout(getQValues, 10);
   }
 }
 
-function getReward(ballPos, targetPos) {
-  return 1 / (1 + 2 * dist(ballPos[0], ballPos[1], targetPos[0], targetPos[1]));
+function actionNum(action) {
+  if (action == "l") {
+    return 0;
+  }
+  else {
+    return 1;
+  }
 }
 
-
-function updateBall(states, model, rand, actions) {
-  let lastState = states[states.length - 1];
-  let input = [];
-  for (let i = 0; i < actions.length; i ++) {
-    input.push(getModelInputArr(lastState, actions[i], actions));
+function makeTensors() {
+  if (xt) {
+    xt.dispose();
   }
-  let predRewards = model.predict(tf.tensor(input)).dataSync();
-  let action = getAction(actions, predRewards, rand);
-  let lastPos = lastState['ball_pos'];
-  let nextPos = [];
-
-  if (action == 'up') {
-    nextPos = [lastPos[0], lastPos[1] - posUpdAmnt];
-  }
-  else if (action == 'down') {
-    nextPos = [lastPos[0], lastPos[1] + posUpdAmnt];
-  }
-  else if (action == 'left') {
-    nextPos = [lastPos[0] - posUpdAmnt, lastPos[1]];
-  }
-  else if (action == 'right') {
-    nextPos = [lastPos[0] + posUpdAmnt, lastPos[1]];
+  if (yt) {
+    yt.dispose();
   }
 
-  states[states.length - 1]['action'] = action;
-  states[states.length - 1]['reward'] = getReward(nextPos, lastState['target_pos']);
-  states.push({
-    'ball_pos': nextPos,
-    'target_pos': lastState['target_pos']
-  });
-
-  return states;
-}
-
-function getXYs(states, actions){
   let xs = [];
   let ys = [];
-  for (let i = 0; i < states.length - 1; i ++) {
-    xs.push(getModelInputArr(states[i], states[i]['action'], actions));
-    ys.push([states[i]['reward']]);
+
+  for (let i = max(0, games.length - numTrainGames); i < games.length; i ++) {
+  // if (games.length > 1 && games[games.length - 1]["ended"]) {
+    let game = games[i]; // games[games.length - 1];
+    for (let j = 0; j < game["states"].length - 1; j ++) {
+      let currState = game["states"][j];
+      xs.push([
+        currState["player pos"] / boardLength,
+        currState["target pos"] / boardLength,
+        actionNum(game["actions"][j])]);
+
+      let nextQValues = [0, 0];
+      if (j < game["states"].length - 2) {
+        nextQValues = game["qvalues"][j + 1];
+      }
+
+      ys.push([game["rewards"][j] + discount * max(...nextQValues)]);
+    }
   }
-  return [xs, ys];
+
+  if (xs.length > 0 && ys.length > 0) {
+    xt = tf.tensor(xs);
+    yt = tf.tensor(ys);
+  }
+
+  setTimeout(makeTensors, 10);
 }
 
-function getLoss(model, states, actions) {
-  let [xs, ys] = getXYs(states, actions);
-  let predictions = model.predict(tf.tensor(xs));
-  return tf.losses.meanSquaredError(tf.tensor(ys), predictions).dataSync();
+function trainModel() {
+  if (xt && yt) {
+    tf.tidy(() => {
+      model.fit(xt, yt).then(result => {losses.push(result.history.loss[0])});
+    });
+  }
+
+  setTimeout(trainModel, 10);
+}
+
+function getNewPos(action) {
+  if (action == "l" && playerPos > 0) {
+    return playerPos - 1;
+  }
+  else if (action == "r" && playerPos < boardLength - 1) {
+    return playerPos + 1;
+  }
+  else {
+    return playerPos;
+  }
+}
+
+function getReward() {
+  if (playerPos == targetPos) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+function getAction(qvalues, randFactor) {
+  if (random() < randFactor) {
+    return randomAction();
+  }
+  else {
+    if (qvalues[0] > qvalues[1]) {
+      return "l";
+    }
+    else {
+      return "r";
+    }
+  }
 }
 
 
-function trainModel(model, states, actions) {
-  let [xs, ys] = getXYs(states, actions);
-  optimizer.minimize(() => {
-    let pred = model.predict(tf.tensor(xs));
-    return tf.losses.meanSquaredError(tf.tensor(ys), pred);
-  })
+function randomAction() {
+  if (random() < 0.5) {
+    return "l";
+  }
+  else {
+    return "r";
+  }
+}
+
+function drawBoard(x, y, w, h, discrete = false) {
+  if (modelQValues) {
+    let maxQValue = max(...modelQValues);
+    let minQValue = min(...modelQValues);
+    for (let i = 0; i < plotModelQValues.length; i ++) {
+      for (let j = 0; j < boardLength; j ++) {
+        let relQValue = map(
+          modelQValues[2 * (boardLength * i + j)] - modelQValues[2 * (boardLength * i + j) + 1],
+          minQValue - maxQValue,
+          maxQValue - minQValue,
+          0,
+          1
+        )
+        noStroke();
+        if (discrete) {
+          if (modelQValues[2 * (boardLength * i + j)] > modelQValues[2 * (boardLength * i + j) + 1]) {
+            fill(myDarkColors[1]);
+          }
+          else {
+            fill(myDarkColors[2]);
+          }
+        }
+        else {
+          fill(makeInbetweenColor(myDarkColors[2], myDarkColors[1], relQValue));
+        }
+        rect(x + j * w / boardLength, y + i * 25, w / boardLength - 2, 20);
+      }
+      fill(myDarkColors[0]);
+      stroke(myDarkColors[0]);
+      strokeWeight(3);
+      rect(x + plotModelQValues[i] * w / boardLength, y + i * 25, w / boardLength - 2, 20);
+    }
+  }
 }
 
 function draw() {
   background(myLightColors[0]);
-  drawBall(states);
-  drawTarget(states);
-  states = updateBall(states, model, rand, actions);
-  trainModel(model, states, actions);
 
-  let currLoss = getLoss(model, states, actions);
-  losses.push(currLoss);
-  if (currLoss > maxLoss) {
-    maxLoss = currLoss;
-  }
-  plot(losses, maxLoss, 0, 0, width / 4, height / 4, myDarkColors[3]);
+  drawBoard(0, 150, width, height, discrete = false);
 
-  let currReward = states[states.length - 2]['reward'];
-  rewards.push(currReward);
-  if (currReward > maxReward) {
-    maxReward = currReward;
-  }
-  plot(rewards, maxReward, 0, height / 4 + 20, width / 4, height / 4, myDarkColors[4]);
+  drawBoard(0, 275, width, height, discrete = true);
 
+  // plot losses
+  plot(losses, maxLossPlot, 10, 10, width / 4, height / 4, myDarkColors[3]);
 
-  rand = rand / randDecayFactor;
+  text(games.length, 100, 100);
 }
