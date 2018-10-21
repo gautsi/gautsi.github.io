e87ba02419671045866903a6f06ca0487f7becc5
Gautam <gautam.sisodia@gmail.com>
2018-10-21 13:56:45 -0400

    updates to map

--- sketches/ny_map/ny_map.js
+++ sketches/ny_map/ny_map.js
@@ -1,5 +1,6 @@
 function preload() {
-  nyMapData = loadJSON("https://raw.githubusercontent.com/gautsi/gen-purpose-repo/master/maps/ny_map_data.jsonhttps://raw.githubusercontent.com/gautsi/gen-purpose-repo/master/maps/ny_map_data_res80.json");
+  // nyMapData = loadJSON("https://raw.githubusercontent.com/gautsi/gen-purpose-repo/master/maps/ny_map_data_res80.json");
+  nyUntangledMapData = loadJSON("https://raw.githubusercontent.com/gautsi/gen-purpose-repo/master/maps/untangled_norm_county_squares_ver2.json");
 }
 
 function setup() {
@@ -9,47 +10,31 @@ function setup() {
 }
 
 function drawMap(mapData) {
-
-  // get max and min lat and lng values
-  let maxLat = mapData[0]["lat"];
-  let minLat = mapData[0]["lat"];
-  let maxLng = mapData[0]["lng"];
-  let minLng = mapData[0]["lng"];
-
-  for (let i = 0; i < Object.keys(mapData).length; i ++) {
-    if (mapData[i]["lat"] > maxLat) {
-      maxLat = mapData[i]["lat"];
-    }
-    if (mapData[i]["lat"] < minLat) {
-      minLat = mapData[i]["lat"];
-    }
-    if (mapData[i]["lng"] > maxLng) {
-      maxLng = mapData[i]["lng"];
-    }
-    if (mapData[i]["lng"] < minLng) {
-      minLng = mapData[i]["lng"];
-    }
-  }
-
-  console.log("minmax done");
-
   noStroke();
 
   for (let i = 0; i < Object.keys(mapData).length; i ++) {
-    let y = map(mapData[i]["lat"], minLat, maxLat, width - 10, 10);
-    let x = map(mapData[i]["lng"], minLng, maxLng, 10, height - 10);
-    if (mapData[i]["state"] == "NY") {
-      fill(myDarkColors[1]);
-    }
-    else {
-      fill(myLightColors[0]);
-    }
+    let y = map(mapData[i]["norm_lat_rank"], 0, 1, width - 10, 10);
+    let x = map(mapData[i]["norm_lng_rank"], 0, 1, 10, height - 10);
+
+    fill(myLightColors[(mapData[i]["county_id"] % 6)]);
+    rectMode(CENTER);
     rect(x, y, 4, 4);
+
+    let min_y = map(mapData[i]["norm_min_new_test_lat_rank"], 0, 1, width - 10, 10);
+    let max_y = map(mapData[i]["norm_max_new_test_lat_rank"], 0, 1, width - 10, 10);
+    let min_x = map(mapData[i]["norm_min_new_test_lng_rank"], 0, 1, 10, height - 10);
+    let max_x = map(mapData[i]["norm_max_new_test_lng_rank"], 0, 1, 10, height - 10);
+
+    fill(myDarkColors[(mapData[i]["county_id"] % 6)]);
+    rectMode(CORNERS);
+    rect(min_x, min_y, max_x, max_y);
+
+
   }
 }
 
 function draw() {
   background(myLightColors[0]);
-  drawMap(nyMapData);
+  drawMap(nyUntangledMapData);
   noLoop();
 }
