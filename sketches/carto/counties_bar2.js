b1de237ade500849fbd5b002beeffe2a91be34cc
gautsi <gautam.sisodia@gmail.com>
2019-03-29 08:12:54 -0400

    added bar2 script to carto

--- sketches/carto/counties_bar2.js
+++ sketches/carto/counties_bar2.js
@@ -1,27 +1,33 @@
-let barStore = {};
+let barStore2 = {};
 
-let grey = "#dddddd";
+let grey2 = "#dddddd";
 
-function loadBarData() {
+function loadBar2Data() {
     return Promise.all([
       d3.csv("https://raw.githubusercontent.com/gautsi/gautsi.github.io/master/sketches/carto/nys_ag_dem_pri_votes_032619.csv")
     ]).then(datasets => {
-        barStore.votes = datasets[0];
-        return barStore;
+        barStore2.votes = datasets[0];
+        return barStore2;
     })
 }
 
-function prepData(data) {
-  data["candidates"] = Object.keys(data.votes[0]).filter(i => i!= "County");
+function prepData2(data) {
+  data["candidates"] = Object.keys(data.votes[0]).filter(i => i != "County");
   data.votes.forEach(i => {
     i["countyFixed"] = i["County"].split(" County")[0];
     i["candidates"] = data["candidates"].map(j => {
       return {candidate: j.split(" (DEM)")[0], votes: Number(i[j].replace(",", ""))}});
     i["totalVotes"] = d3.sum(i.candidates.map(a => a.votes));
   });
+
+  data.layers = d3.stack()(data.candidates.map(function(c) {
+    return data.votes.map(function(d) {
+      return {x: d.candidates.filter(i => i["candidate"] === c.split(" (DEM)")[0])[0].votes, y: d.countyFixed};
+    });
+  }));
 }
 
-function getBarConfig() {
+function getBarConfig2() {
     let width = 400;
     let height = 600;
     let margin = {
@@ -32,7 +38,7 @@ function getBarConfig() {
     }
     let bodyHeight = height - margin.top - margin.bottom
     let bodyWidth = width - margin.left - margin.right
-    let container = d3.select("#bar1").append("svg")
+    let container = d3.select("#bar2").append("svg")
 
     container
         .attr("width", width)
@@ -41,7 +47,7 @@ function getBarConfig() {
     return { width, height, margin, bodyHeight, bodyWidth, container }
 }
 
-function getBarScales(config, data) {
+function getBarScales2(config, data) {
   let { width, height, margin, bodyHeight, bodyWidth, container } = config;
 
   let maximumCount = d3.max(data.votes.map(a => a.totalVotes));
@@ -62,7 +68,7 @@ function getBarScales(config, data) {
 
 
 
-function drawBar(config, scales, data, color = d3.schemeDark2[1], fill = "none") {
+function drawBar2(config, scales, data, color = d3.schemeDark2[1], fill = "none") {
   let { width, height, margin, bodyHeight, bodyWidth, container } = config;
   let { xScale, yScale } = scales;
 
@@ -83,12 +89,12 @@ function drawBar(config, scales, data, color = d3.schemeDark2[1], fill = "none")
     .attr("height", d => yScale.bandwidth())
     // .attr("stroke-width", 2)
     .attr("stroke", "none")
-    .attr("fill", grey);
+    .attr("fill", grey2);
 
   nodes.exit().remove();
 }
 
-function drawAxesBarChart(config, scales, data){
+function drawAxesBarChart2(config, scales, data){
   let {xScale, yScale} = scales
   let {container, margin, height} = config;
   let axisX = d3.axisBottom(xScale)
@@ -110,13 +116,13 @@ function drawAxesBarChart(config, scales, data){
 }
 
 
-function showBarData(data) {
-  prepData(data);
+function showBarData2(data) {
+  prepData2(data);
   console.log(data);
-  let config = getBarConfig();
-  let scales = getBarScales(config, data);
-  drawBar(config, scales, data);
-  drawAxesBarChart(config, scales, data);
+  let config = getBarConfig2();
+  let scales = getBarScales2(config, data);
+  drawBar2(config, scales, data);
+  drawAxesBarChart2(config, scales, data);
 }
 
-loadBarData().then(showBarData);
+loadBar2Data().then(showBarData2);
