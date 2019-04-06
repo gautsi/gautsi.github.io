1e228733e1812fd988dbddd7f73152c6fab4c5d8
gautsi <gautam.sisodia@gmail.com>
2019-04-06 15:53:44 -0400

    updates to neighbors

--- sketches/neighbors/neighbors.js
+++ sketches/neighbors/neighbors.js
@@ -1,17 +1,21 @@
 function neighborsConfig() {
   let config = {
-    "width": 400,
-    "height": 400,
-    "margin": {
-      "top": 10,
-      "bottom": 10,
-      "left": 10,
-      "right": 10
+    width: 400,
+    height: 400,
+    margin: {
+      top: 10,
+      bottom: 10,
+      left: 10,
+      right: 10
     },
-    "numRows": 10,
-    "numCols": 10,
-    "container": d3.select("#neighbors").append("svg"),
-    "grey": "#cccccc"
+    numRows: 10,
+    numCols: 10,
+    container: d3.select("#neighbors").append("svg"),
+    grey: "#cccccc",
+    startingPositions: [
+      { player : 1, positions: [[0, 4], [0, 5], [1, 4]] },
+      { player : 2, positions: [[9, 4], [9, 5], [8, 5]] }],
+    rules: [1, 1]
   }
 
   config.bodyHeight = config.height - config.margin.top - config.margin.bottom;
@@ -37,24 +41,66 @@ function neighborsConfig() {
 
   config.board = [];
   d3.range(config.numRows).forEach(r => d3.range(config.numCols).forEach(c => {
-    config.board.push({ "row": r, "col": c, "player": 0 });
+    config.board.push({ "row": r, "col": c, "player": 0, "neighbors": [0, 0] });
+  }));
+
+  config.startingPositions.forEach(ps => ps.positions.forEach(p => {
+    config.board[p[0] * config.numRows + p[1]].player = ps.player;
   }));
 
   return config;
 }
 
 function drawBoard(config) {
+  calcNeighbors(nConfig);
+
   config.boardJoin = config.container
-    .selectAll("rect")
+    .selectAll(".piece")
     .data(config.board)
     .enter()
     .append("rect")
+    .attr("class", "piece")
     .attr("x", d => config.xScale(d.col))
     .attr("y", d => config.yScale(d.row))
     .attr("width", config.xScale.bandwidth())
     .attr("height", config.yScale.bandwidth())
     .attr("stroke", "none")
     .attr("fill", d => config.colorScale(d.player));
+
+    config.textJoin = config.container
+      .selectAll("text")
+      .data(config.board)
+      .enter()
+      .append("text")
+      .text(d => d.neighbors.reduce((a, b) => a + ", " + b))
+      .attr("x", d => config.xScale(d.col) + config.xScale.bandwidth() / 8)
+      .attr("y", d => config.yScale(d.row) + config.yScale.bandwidth() / 2);
+
+    config.textJoin.exit().remove();
+}
+
+function calcNeighbors(config) {
+  config.board.forEach(d => {
+    d.neighbors = [0, 0];
+  });
+
+  config.board.forEach((d, i) => {
+    if (d.player > 0) {
+      let row = Math.floor(i / config.numRows);
+      let col = i % config.numCols;
+      let neighborPos = [-1, 0, 1]
+        .flatMap(j => [-1, 0, 1].map(
+          k => [row + j, col + k]))
+        .filter(l => l[0] >= 0 && l[0] < config.numRows && l[1] >= 0 && l[1] < config.numCols)
+        .map(l => l[0] * config.numRows + l[1]);
+
+      neighborPos.forEach(p => {config.board[p].neighbors[d.player - 1] += 1});
+    }
+  });
+}
+
+function randomizeRules(config) {
+  config.rules = [0, 1].map(i => 1 + Math.floor(2 * Math.random()));
 }
 
 function randomizeBoard(board) {
@@ -63,13 +109,41 @@ function randomizeBoard(board) {
   });
 }
 
+function drawBackground(config) {
+  config.container
+    .append("rect")
+    .attr("class", "background")
+    .attr("fill", "white")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", config.width)
+    .attr("height", config.height)
+    .attr("stroke", "black");
+}
+
+function updateBoard(config) {
+  config.board.forEach(p => {
+    if (p.neighbors[0] == config.rules[0] && p.neighbors[1] != config.rules[1]) {
+        p.player = 1;
+    } else if (p.neighbors[1] == config.rules[1] && p.neighbors[0] != config.rules[0]) {
+      p.player = 2;
+    } else {
+      p.player = 0;
+    }
+  });
+}
+
 function runGame(config) {
   let gameTimer = d3.interval(elapsed => {
-    randomizeBoard(config.board);
-    config.boardJoin.transition().duration(1000).attr("fill", d => config.colorScale(d.player));
-  }, 2000);
+    randomizeRules(config);
+    updateBoard(config);
+    calcNeighbors(config);
+    config.boardJoin.transition().duration(500).attr("fill", d => config.colorScale(d.player));
+    config.textJoin.transition().duration(500).text(d => d.neighbors.reduce((a, b) => a + ", " + b));
+  }, 1000);
 }
 
 let nConfig = neighborsConfig();
+// drawBackground(nConfig);
 drawBoard(nConfig);
 runGame(nConfig);
