ac0a341289c0a822f06765be058966501f94115d
Gautam Sisodia <gautam.sisodia@gmail.com>
2020-11-14 14:21:28 -0500

    refactor to instance mode

--- sketches/tiling/tiling.js
+++ sketches/tiling/tiling.js
@@ -1,111 +1,105 @@
-function setup() {
-    let myCanvas = createCanvas(400, 400);
-    myCanvas.parent('tiling');
-    angleMode(RADIANS);
-    rectMode(CENTER);
-}
-
 let sideLength = 20;
-let shape;
-let rot = 0;
-let x = 0;
-let y = 0;
-let angle = 0;
-let actions = [];
 
-function switchSide() {
+function switchSide(sketch, shape, rot) {
     if (shape == 'square') {
         if (rot == 0) {
-            rotate(PI/2);
-            // drawReference(myDarkColors[2]);
-            // drawSquare();
+            sketch.rotate(sketch.PI / 2);
         }
         else {
-            translate(0, -sideLength);
-            rotate(-PI/2);
-            // drawReference(myDarkColors[2]);
-            // drawSquare();
+            sketch.translate(0, -sideLength);
+            sketch.rotate(-sketch.PI / 2);
         }
     }
     else if (shape == 'triangle') {
         if (rot == 0) {
-            rotate(PI/3);
-            // drawReference(myDarkColors[2]);
-            // drawTriangle();
+            sketch.rotate(sketch.PI / 3);
         }
         else {
-            translate(0, -sideLength);
-            rotate(-2*PI/3);
-            // drawReference(myDarkColors[2]);
-            // drawTriangle();
-
+            sketch.translate(0, -sideLength);
+            sketch.rotate(-2 * sketch.PI / 3);
         }
     }
 }
 
+function drawTriangle(sketch) {
+    sketch.strokeWeight(1);
+    sketch.stroke(myDarkColors[1]);
+    sketch.fill(myDarkColors[0]);
+    sketch.triangle(0, 0, 0, -sideLength, sideLength * sketch.sqrt(3) / 2, -sideLength * 1 / 2);
+}
 
-function draw() {
-    background(myLightColors[0]);
-    translate(width / 2, height / 2);
-    for (let i = 0; i < actions.length; i++){
-        if (actions[i] == 'square') {
-            drawSquare();
-            shape = 'square';
-            rot = 0;
+function drawSquare(sketch) {
+    sketch.strokeWeight(1);
+    sketch.stroke(myDarkColors[1]);
+    sketch.fill(myDarkColors[0]);
+    sketch.square(sideLength / 2, -sideLength / 2, sideLength);
+}
+
+function drawReference(sketch, c) {
+    sketch.strokeWeight(3);
+    sketch.stroke(c);
+    sketch.line(0, 0, 0, -sideLength);
+    sketch.strokeWeight(5);
+    sketch.point(0, 0);
+}
+
+
+function create(sketch) {
+    let shape;
+    let rot = 0;
+    let x = 0;
+    let y = 0;
+    let angle = 0;
+    let actions = [];
+
+    sketch.setup = () => {
+        sketch.createCanvas(400, 400);
+        sketch.angleMode(sketch.RADIANS);
+        sketch.rectMode(sketch.CENTER);
+    }
+    sketch.draw = () => {
+        sketch.background(myLightColors[0]);
+        sketch.translate(sketch.width / 2, sketch.height / 2);
+        for (let i = 0; i < actions.length; i++) {
+            if (actions[i] == 'square') {
+                drawSquare(sketch);
+                shape = 'square';
+                rot = 0;
+            }
+            else if (actions[i] == 'triangle') {
+                drawTriangle(sketch);
+                shape = 'triangle';
+                rot = 0;
+            }
+            if (actions[i] == 'switch') {
+                switchSide(sketch, shape, rot);
+                rot += 1;
+            }
         }
-        else if (actions[i] == 'triangle') {
-            drawTriangle();
-            shape = 'triangle';
-            rot = 0;
+        drawReference(sketch, myDarkColors[2]);
+    }
+
+    sketch.keyPressed = () => {
+        // s for "square"
+        if (sketch.keyCode == 83) {
+            actions.push('square');
         }
-        if (actions[i] == 'switch') {
-            switchSide();
-            rot += 1;
+
+        // t for "triangle"
+        if (sketch.keyCode == 84) {
+            actions.push('triangle');
+        }
+
+        // r for "rotate" (switch side)
+        if (sketch.keyCode == 82) {
+            actions.push('switch');
+        }
+
+        // d for "delete"
+        if (sketch.keyCode == 68) {
+            actions.pop();
         }
     }
-    drawReference(myDarkColors[2]);
 }
 
-function drawTriangle() {
-    strokeWeight(1);
-    stroke(myDarkColors[1]);
-    fill(myDarkColors[0]);
-    triangle(0, 0, 0, -sideLength, sideLength * sqrt(3) / 2, -sideLength * 1 / 2);
-}
-
-function drawSquare() {
-    strokeWeight(1);
-    stroke(myDarkColors[1]);
-    fill(myDarkColors[0]);
-    square(sideLength / 2, -sideLength / 2, sideLength);
-}
-
-function drawReference(c) {
-    strokeWeight(3);
-    stroke(c);
-    line(0, 0, 0, -sideLength);
-    strokeWeight(5);
-    point(0, 0);
-}
-
-function keyPressed() {
-    // s for "square"
-    if (keyCode == 83) {
-        actions.push('square');
-    }
-  
-    // t for "triangle"
-    if (keyCode == 84) {
-        actions.push('triangle');
-    }
-
-    // r for "rotate" (switch side)
-    if (keyCode == 82) {
-        actions.push('switch');
-    }
-    
-    // d for "delete"
-    if (keyCode == 68) {
-        actions.pop();
-    }
-  }
+let createp5 = new p5(create, 'tiling');
