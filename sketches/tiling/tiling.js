f4dda42b7c68fddcb9c49347850655f246bbe3f1
Gautam Sisodia <gautam.sisodia@gmail.com>
2020-11-15 00:13:48 -0500

    upd to auto

--- sketches/tiling/tiling.js
+++ sketches/tiling/tiling.js
@@ -1,4 +1,5 @@
 let sideLength = 20;
+let shapeColors = [myDarkColors[0], myDarkColors[2]]
 
 function switchSide(sketch, shape, rot) {
     if (shape == 'square') {
@@ -21,17 +22,18 @@ function switchSide(sketch, shape, rot) {
     }
 }
 
-function drawTriangle(sketch) {
-    sketch.strokeWeight(1);
+function drawShape(sketch, color, shapeFunc) {
+    sketch.strokeWeight(2);
     sketch.stroke(myDarkColors[1]);
-    sketch.fill(myDarkColors[0]);
+    sketch.fill(color);
+    shapeFunc(sketch);
+}
+
+function drawTriangle(sketch) {
     sketch.triangle(0, 0, 0, -sideLength, sideLength * sketch.sqrt(3) / 2, -sideLength * 1 / 2);
 }
 
 function drawSquare(sketch) {
-    sketch.strokeWeight(1);
-    sketch.stroke(myDarkColors[1]);
-    sketch.fill(myDarkColors[0]);
     sketch.square(sideLength / 2, -sideLength / 2, sideLength);
 }
 
@@ -46,45 +48,54 @@ function drawReference(sketch, c) {
 function drawActions(sketch, actions, num_actions) {
     let shape;
     let rot = 0;
+    let colorIndex = 0;
     for (let i = 0; i < min(num_actions, actions.length); i++) {
-        if (actions[i] == 'square') {
-            drawSquare(sketch);
+        if (actions[i] == 's') {
+            drawShape(sketch, shapeColors[colorIndex], drawSquare);
             shape = 'square';
             rot = 0;
         }
-        else if (actions[i] == 'triangle') {
-            drawTriangle(sketch);
+        else if (actions[i] == 't') {
+            drawShape(sketch, shapeColors[colorIndex], drawTriangle);
             shape = 'triangle';
             rot = 0;
         }
-        if (actions[i] == 'switch') {
+        if (actions[i] == 'r') {
             switchSide(sketch, shape, rot);
             rot += 1;
         }
+        if (actions[i] == 'c') {
+            colorIndex = 1 - colorIndex;
+        }
     }
-    drawReference(sketch, myDarkColors[2]);
+    drawReference(sketch, myLightColors[3]);
 }
 
-function readKey(sketch, actions) {
+function readKey(sketch) {
     return () => {
         // s for "square"
         if (sketch.keyCode == 83) {
-            actions.push('square');
+            sketch.actions += 's';
         }
 
         // t for "triangle"
         if (sketch.keyCode == 84) {
-            actions.push('triangle');
+            sketch.actions += 't';
         }
 
         // r for "rotate" (switch side)
         if (sketch.keyCode == 82) {
-            actions.push('switch');
+            sketch.actions += 'r';
         }
 
         // d for "delete"
         if (sketch.keyCode == 68) {
-            actions.pop();
+            sketch.actions = sketch.actions.slice(0, -1);
+        }
+        
+        // c to change "color"
+        if (sketch.keyCode == 67) {
+            sketch.actions += 'c';
         }
     }
 }
@@ -99,28 +110,40 @@ function setup(sketch, fr) {
 }
 
 function create(sketch) {
-    let actions = [];
+    sketch.actions = '';
 
     sketch.setup = setup(sketch, 60);
     sketch.draw = () => {
         sketch.background(myLightColors[0]);
         sketch.translate(sketch.width / 2, sketch.height / 2);
-        drawActions(sketch, actions, actions.length);
+        drawActions(sketch, sketch.actions, sketch.actions.length);
     }
 
-    sketch.keyPressed = readKey(sketch, actions);
+    sketch.keyPressed = readKey(sketch);
 }
 
 function auto(sketch) {
-    let actions = ['triangle', 'switch', 'triangle', 'switch'];
-    let action_num = 0;
+    sketch.actions = [
+        Array(6).fill('tr'),
+        ['crsrt'],
+        Array(5).fill('rsrrt'),
+        ['crrtrs'],
+        Array(11).fill('rtrrs'),
+        ['crrtrtrrtrs'],
+        Array(11).fill('rtrrtrrtrs'),
+        ['crrsrrtrtrrtrt'],
+        Array(11).fill('rsrrrtrtrrtrt'),
+        ['crrtrsrrsrrs'],
+        Array(11).fill('rtrrsrrsrrs'),
+        ['crr']].flat().join('');
+    sketch.action_num = 0;
 
-    sketch.setup = setup(sketch, 10);
+    sketch.setup = setup(sketch, 120);
     sketch.draw = () => {
         sketch.background(myLightColors[0]);
         sketch.translate(sketch.width / 2, sketch.height / 2);
-        drawActions(sketch, actions, action_num);
-        action_num += 1;
+        drawActions(sketch, sketch.actions, sketch.action_num);
+        sketch.action_num += 1;
     }
 }
 
